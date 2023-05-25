4fabf217394419d1a3d0c46964767fdc5593635f
laiba <laibazafar1301@gmail.com>
2023-05-25 11:29:02 +0500

    completed

--- problems.py
+++ problems.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 # import numpy as np
 import sys
-import math 
+import math
 
 
 def find_quotiant(a, b):
@@ -45,10 +45,10 @@ def check_number(num):
 
 
 def check_divisible():
-    #fixme what of diviser = 0 ?
+    # fixme what of diviser = 0 ?
     num = int(input('Give a number: '))
     diviser = int(input("Check if it's divisble on: "))
-    if num == 0  or diviser == 0:
+    if num == 0 or diviser == 0:
         print("0 can't be a valid divisor or divident")
     elif num % diviser == 0:
         print("its divideable")
@@ -57,8 +57,8 @@ def check_divisible():
 
 
 def sum_of_odd():
-    #fixme if user enter even numbers ?
-    total = 0 
+    # fixme if user enter even numbers ?
+    total = 0
     odd_counter = 0
     while odd_counter < 10:
         number = int(input('Enter a Number'))
@@ -71,7 +71,7 @@ def sum_of_odd():
 
 
 def sum_of_even():
-    #fixme if user enter odd numbers ?
+    # fixme if user enter odd numbers ?
     range_of_num = int(input("How many even numbers you wanna give: "))
     total = 0
     even_counter = 0
@@ -81,7 +81,7 @@ def sum_of_even():
             total += number
             even_counter += 1
         else:
-             continue
+            continue
     print(total)
 
 
@@ -193,7 +193,7 @@ def calculate_grade():
 
 
 def check_even_odd():
-    #fixme if user enter 0?
+    # fixme if user enter 0?
     num = int(input("Enter a number: "))
     if num % 2 == 0:
         print('Number is even')
@@ -236,13 +236,13 @@ def average_of_n():
 
 
 def find_range(n):
-    for i in range(0, n ):
+    for i in range(0, n):
         print(i)
 
 
 def find_fictorial(n):
     fictorial = 1
-    # enuirate try this 
+    # enuirate try this
     for i in range(2, n + 1):
         fictorial *= i
     print(fictorial)
@@ -279,6 +279,7 @@ def largest_n_number():
             largest = numbers
     print(largest)
 
+
 def find_small_and_large():
     n = int(input("How many numbers you want to add: "))
     numbers = []
@@ -290,12 +291,13 @@ def find_small_and_large():
     for number in numbers:
         if number > largest:
             largest = number
-    
+
     smallest = numbers[0]
     for number in numbers:
         if number < smallest:
             smallest = number
     print(largest, smallest)
+
 
 def num_of_negative_or_positive(n):
     num_of_positive = 0
@@ -311,55 +313,59 @@ def num_of_negative_or_positive(n):
             num_of_negative += 1
     print(f"You gave {num_of_positive} positive and {num_of_negative} negative integers")
 
+
 def find_divisor():
     n = int(input("Enter a number: "))
     divisors = []
-    for i in range(1,n+1):
-       if n % i == 0:
-          divisors.append(i)
+    for i in range(1, n + 1):
+        if n % i == 0:
+            divisors.append(i)
     print(divisors)
-    return divisors 
-           
+    return divisors
+
 
 def find_perfect_positive():
     n = int(input("Enter a number: "))
     divisors = []
-    for i in range(1,n):
-       if n % i == 0:
-          divisors.append(i)
-   
+    for i in range(1, n):
+        if n % i == 0:
+            divisors.append(i)
+
     if sum(divisors) == n:
         print("Perfect Positive Number")
     else:
         print('Not a perfect positive Number')
 
+
 def find_prime():
     n = int(input("Enter a number: "))
-    for i in range(2,n):
-       if n % i == 0:
-          print("Not a prime number")
-          break 
+    for i in range(2, n):
+        if n % i == 0:
+            print("Not a prime number")
+            break
     print("Prime Number")
 
+
 def take_positive_only():
-     not_positive = True
-     while not_positive:
+    not_positive = True
+    while not_positive:
         n = int(input("Enter a number:"))
         if n > 0:
             not_positive = False
             print("Its Positive")
-        else: 
+        else:
             print('Not Positive', file=sys.stderr)
+
 
 def sum_until_sentinal():
     sentinal_prov = False
     numbers = []
-    while sentinal_prov == False :
-         n = int(input("Enter a number:"))
-         if n == -999:
-             sentinal_prov = True
-         else:
-             numbers.append(n)
+    while sentinal_prov == False:
+        n = int(input("Enter a number:"))
+        if n == -999:
+            sentinal_prov = True
+        else:
+            numbers.append(n)
     print(sum(numbers))
 
 
@@ -371,7 +377,7 @@ def display_negative():
 
 def find_abs():
     n = int(input('Enter a number to make it absolute:'))
-    if n >= 0 :
+    if n >= 0:
         print(f"Absolute Value is {n}")
     else:
         absolute = n - (n + n)
@@ -379,18 +385,17 @@ def find_abs():
 
 
 def find_two_even_odd():
-   
     n1 = int(input('Enter first number: '))
     n2 = int(input("Enter second number: "))
-    if n1 % 2 == 0 and n2%2 == 0:
+    if n1 % 2 == 0 and n2 % 2 == 0:
         print("Both are even")
-    elif n1 % 2 != 0 and n2%2 != 0:
+    elif n1 % 2 != 0 and n2 % 2 != 0:
         print('Both are odd')
-    else :
+    else:
         print("One is even and One is odd")
 
 
-def  find_odd_from3():
+def find_odd_from3():
     numbers = []
     no_of_odd = 0
     for i in range(3):
@@ -400,41 +405,41 @@ def  find_odd_from3():
         if number % 2 != 0:
             no_of_odd += 1
     print(f'There are {no_of_odd} odd numbers')
-       
+
 
 def find_2_largest():
     numbers = []
-    
+
     for i in range(3):
         n = int(input("Enter a number: "))
         numbers.append(n)
     smallest = numbers[0]
     for number in numbers:
-            if number < smallest:
-                smallest = number
+        if number < smallest:
+            smallest = number
     smallest_index = numbers.index(smallest)
     numbers.pop(smallest_index)
     print(f"Two largest numbers are {numbers}")
 
 
-def  find_2digit_int():
+def find_2digit_int():
     n = int(input('Enter a number: '))
     if len(str(n)) == 2:
         print("It is a 2 digit number.")
-    else :
+    else:
         print("Not a valid two digit number")
 
 
-def   find_abs_diff_2digit():
+def find_abs_diff_2digit():
     n = input("Enter a two digit number: ")
     abs_diff = abs(int(n[0]) - int(n[1]))
     print(abs_diff)
 
 
-def  reverse_integer():
+def reverse_integer():
     # todo without using list comprehension
     number = input('Enter a number: ')
-    length = len(number) - 1 
+    length = len(number) - 1
     reverse_int = ''
     while True:
         if length < 0:
@@ -443,22 +448,25 @@ def  reverse_integer():
         length -= 1
     print(reverse_int)
 
+
 def interchange_numbers():
     a = int(input("Enter First number:"))
     b = int(input("Enter Second number:"))
-    c = a 
+    c = a
     a = b
-    b = c 
-    print(a,b)
+    b = c
+    print(a, b)
+
 
 def interchange_without_var():
-    #fixme me without using python function
+    # fixme me without using python function
     a = int(input('Enter a first Number:'))
     b = int(input('Enter Second Number:'))
-    b, a = a,b 
-    print(a,b)
+    b, a = a, b
+    print(a, b)
 
-def  multiply_sum_digit():
+
+def multiply_sum_digit():
     n = input('Enter a Multi-digit number:')
     digits = []
     for digit in n:
@@ -473,68 +481,75 @@ def first_divisible_second():
     if first_num % second_num == 0:
         print('YES')
 
+
 def second_divisible_first():
     first_num = int(input('Enter First Number:'))
     second_num = int(input('Enter Second Number:'))
-    if  second_num % first_num == 0:
+    if second_num % first_num == 0:
         print('YES')
 
-def  each_other_divisble():
+
+def each_other_divisble():
     first_num = int(input('Enter First Number:'))
     second_num = int(input('Enter Second Number:'))
-    if  second_num % first_num == 0 or  first_num % second_num == 0:    
+    if second_num % first_num == 0 or first_num % second_num == 0:
         print('YES')
+
 
 def zero_sentinal():
     sentinal_prov = False
     numbers = []
-    while sentinal_prov == False :
-         n = int(input("Enter a number:"))
-         if n == 0:
-             sentinal_prov = True
-         else:
-             numbers.append(n)
+    while sentinal_prov == False:
+        n = int(input("Enter a number:"))
+        if n == 0:
+            sentinal_prov = True
+        else:
+            numbers.append(n)
     print(sum(numbers))
+
 
 def sentinal_last_nonzero():
     sentinal_prov = False
     numbers = []
-    while sentinal_prov == False :
-         n = int(input("Enter a number:"))
-         if n == 0:
-             sentinal_prov = True
-         else:
-             numbers.append(n)
+    while sentinal_prov == False:
+        n = int(input("Enter a number:"))
+        if n == 0:
+            sentinal_prov = True
+        else:
+            numbers.append(n)
     print(numbers[-1])
+
 
 def sentinal_largest():
     sentinal_prov = False
     numbers = []
     largest = 0
-    while sentinal_prov == False :
-         n = int(input("Enter a number:"))
-         if n == 0:
-             sentinal_prov = True
-         else:
-             numbers.append(n)
-             if n > largest:
-                 largest = n
+    while sentinal_prov == False:
+        n = int(input("Enter a number:"))
+        if n == 0:
+            sentinal_prov = True
+        else:
+            numbers.append(n)
+            if n > largest:
+                largest = n
     print(f"Largest Value is: {largest}")
 
+
 def sentinal_smallest():
-   sentinal_prov = False
-   numbers = []
-   while sentinal_prov == False:
-       n = int(input('Enter a number: '))
-       if n == 0:
-           break
-       if len(numbers) == 0:
-           numbers.append(n)
-           smallest = n
-           continue
-       if n < smallest:
-           smallest = n
-   print(smallest)
+    sentinal_prov = False
+    numbers = []
+    while sentinal_prov == False:
+        n = int(input('Enter a number: '))
+        if n == 0:
+            break
+        if len(numbers) == 0:
+            numbers.append(n)
+            smallest = n
+            continue
+        if n < smallest:
+            smallest = n
+    print(smallest)
+
 
 def small_from_10():
     numbers = []
@@ -553,58 +568,61 @@ def even_odd_from_10():
     no_of_even = 0
     for i in range(10):
         n = int(input('Enter a Number:'))
-        if n % 2 == 0 :
+        if n % 2 == 0:
             no_of_even += 1
-        else :
-            no_of_odd += 1 
+        else:
+            no_of_odd += 1
     print(f"Number of Even numbers are: {no_of_even}")
     print(f"Number of Odd numbers are: {no_of_odd}")
+
 
 def even_range_Limit():
     Slimit = int(input('Enter Slimit:')) + 1
     Wlimit = int(input('Enter Elimit:'))
     even_range = []
-    for i in range(Slimit,Wlimit):
+    for i in range(Slimit, Wlimit):
         if i % 2 == 0:
             even_range.append(i)
     print(even_range)
+
 
 def divisble_range():
     Slimit = int(input('Enter Slimit:')) + 1
     Wlimit = int(input('Enter Elimit:'))
     divisble_range = []
-    for i in range(Slimit,Wlimit):
+    for i in range(Slimit, Wlimit):
         if i % 2 == 0 or i % 3 == 0 or i % 5 == 0:
             divisble_range.append(i)
     print(divisble_range)
 
+
 def find_GCD():
-    #fixme do it without using math/numpy
+    # fixme do it without using math/numpy
     num1 = find_divisor()
     num2 = find_divisor()
-    common_divisors = np.intersect1d(num1,num2)
+    common_divisors = np.intersect1d(num1, num2)
     GCD = np.max(common_divisors)
     print(f"GCD of two number is {GCD}")
 
-def  find_three_GCD():
-    #fixme do it without using math/numpy
+
+def find_three_GCD():
+    # fixme do it without using math/numpy
     num1 = find_divisor()
     num2 = find_divisor()
     num3 = find_divisor()
-    common_divisors = np.intersect1d(num1,num2,num3)
+    common_divisors = np.intersect1d(num1, num2, num3)
     GCD = np.max(common_divisors)
     print(f"GCD of three numbers is: {GCD}")
 
 
 def find_lcm():
-    #fixme do it without using math/numpy
+    # fixme do it without using math/numpy
     num1 = int(input('Enter a Number :'))
     num2 = int(input('Enter a Number :'))
-    print(math.lcm(num1,num2))
+    print(math.lcm(num1, num2))
 
 
 def find_digit():
-    
     n = int(input('Enter a Number: '))
     print(len(str(n)))
 
@@ -612,33 +630,35 @@ def find_digit():
 def digits_to_num():
     sentinal_prov = False
     numbers = []
-    while sentinal_prov == False :
-         n = int(input("Enter a number:"))
-         if n == 9:
-             sentinal_prov = True
-         else:
-             numbers.append(n)
+    while sentinal_prov == False:
+        n = int(input("Enter a number:"))
+        if n == 9:
+            sentinal_prov = True
+        else:
+            numbers.append(n)
     decimal = int(''.join(map(str, numbers)))
     print(f"Decimal number is {decimal}")
+
 
 def digits_to_num_reverse():
     sentinal_prov = False
     numbers = []
-    while sentinal_prov == False :
-         n = int(input("Enter a number:"))
-         if n == 9:
-             sentinal_prov = True
-         else:
-             numbers.append(n)
+    while sentinal_prov == False:
+        n = int(input("Enter a number:"))
+        if n == 9:
+            sentinal_prov = True
+        else:
+            numbers.append(n)
     print(numbers)
     numbers = numbers[::-1]
     print(numbers)
     decimal = int(''.join(map(str, numbers)))
     print(f"Decimal number is {decimal}")
 
-def  convert_base_9_obo():
-    #fixme do it without using math/numpy
-    n  = int(input('Enter a Number:'))
+
+def convert_base_9_obo():
+    # fixme do it without using math/numpy
+    n = int(input('Enter a Number:'))
     quotient = n
     converted = False
     remainders = []
@@ -651,90 +671,93 @@ def  convert_base_9_obo():
     for number in remainders:
         print(number)
 
-def  convert_base_9_decimal():
+
+def convert_base_9_decimal():
     digits = []
     sentinal_given = False
     decimal = 0
     power = 0
-    # Getting data one by one 
+    # Getting data one by one
     while sentinal_given == False:
         n = int(input('Enter a Number: '))
-        if n == 9 :
+        if n == 9:
             sentinal_given = True
-        else : 
+        else:
             digits.append(n)
     digits = reversed(digits)
 
-    # Base 9 to decimal 
-    for digit in digits:    
+    # Base 9 to decimal
+    for digit in digits:
         decimal += digit * (9 ** power)
-        power += 1 
+        power += 1
     print(f" Decimal value is : {decimal}")
+
 
 def convertBase9ToBinary():
     digits = []
     sentinal_given = False
     decimal = 0
     power = 0
-    # Getting data one by one 
+    # Getting data one by one
     while sentinal_given == False:
         n = int(input('Enter a Number: '))
-        if n == 9 :
+        if n == 9:
             sentinal_given = True
-        else : 
+        else:
             digits.append(n)
     digits = reversed(digits)
 
-    # Base 9 to decimal 
-    for digit in digits:    
+    # Base 9 to decimal
+    for digit in digits:
         decimal += digit * (9 ** power)
-        power += 1 
+        power += 1
     print(decimal)
     binary = ''
-    quotient = decimal 
+    quotient = decimal
 
-
-    # decimal to binary 
-    while True :
-        quotient  , remainder = divmod (quotient , 2 )
+    # decimal to binary
+    while True:
+        quotient, remainder = divmod(quotient, 2)
         binary += str(remainder)
         if quotient == 1:
             break
     print(f"Base 9 to Binary is : {binary}")
 
 
-
-def convertToHexa(): 
+def convertToHexa():
     remainders = []
     quotient = int(input('Enter a Number: '))
     converted = False
     while converted == False:
-        remainder = quotient % 16 
-        quotient  = int(quotient / 16)
+        remainder = quotient % 16
+        quotient = int(quotient / 16)
         remainders.append(remainder)
-        if quotient == 0 :
-            converted = True 
+        if quotient == 0:
+            converted = True
     remainders = reversed(remainders)
     for remainder in remainders:
-       string_num = str(remainder)
-       num = string_num.replace('10', 'A').replace('11', 'B').replace('12', 'C').replace('13', 'D').replace('14', 'E').replace('15', 'F')
-       print(num)
-
+        string_num = str(remainder)
+        num = string_num.replace('10', 'A').replace('11', 'B').replace('12', 'C').replace('13', 'D').replace('14',
+                                                                                                             'E').replace(
+            '15', 'F')
+        print(num)
 
 
 def ascending_order():
-        A = int(input('Enter a Number'))
-        B = int(input('Enter a Number'))
-        C = int(input('Enter a Number'))
-        num = [A,B,C]
-        num.sort() 
-        print(num)
+    A = int(input('Enter a Number'))
+    B = int(input('Enter a Number'))
+    C = int(input('Enter a Number'))
+    num = [A, B, C]
+    num.sort()
+    print(num)
 
-def  pressure_temp_warning():
+
+def pressure_temp_warning():
     pressure = int(input('Enter Value of Pressure: '))
     temp = int(input('Enter Value of Pressure: '))
     if temp >= 100 or pressure >= 200:
         print('WARNING')
+
 
 def division_mod_without_operator():
     divident = int(input("Enter the divident: "))
@@ -742,18 +765,19 @@ def division_mod_without_operator():
     quotient = 0
     while divident >= divisor:
         divident -= divisor
-        quotient += 1 
-    
+        quotient += 1
+
     print(f"a/b is = {quotient}")
     print(f'Remainder is = {divident}')
 
+
 def mircorseconds_to_other_time():
     microseconds = int(input('Enter micorseconds'))
-    weeks, microseconds = divmod(microseconds, 7 * 24 * 60 * 60 * 10**6)
-    days, microseconds = divmod(microseconds, 24 * 60 * 60 * 10**6)
-    hours, microseconds = divmod(microseconds, 60 * 60 * 10**6)
-    minutes, microseconds = divmod(microseconds, 60 * 10**6)
-    seconds, microseconds = divmod(microseconds, 10**6)
+    weeks, microseconds = divmod(microseconds, 7 * 24 * 60 * 60 * 10 ** 6)
+    days, microseconds = divmod(microseconds, 24 * 60 * 60 * 10 ** 6)
+    hours, microseconds = divmod(microseconds, 60 * 60 * 10 ** 6)
+    minutes, microseconds = divmod(microseconds, 60 * 10 ** 6)
+    seconds, microseconds = divmod(microseconds, 10 ** 6)
     print(f"Weeks: {weeks}")
     print(f"Days: {days}")
     print(f"Hours: {hours}")
@@ -761,21 +785,24 @@ def mircorseconds_to_other_time():
     print(f"Seconds: {seconds}")
     print(f"Microseconds: {microseconds}")
 
-def count_vowels(words) :
+
+def count_vowels(words):
     for word in words:
         original = word
         word = word.lower()
-        vowels = ['a','e','i','o','u']
+        vowels = ['a', 'e', 'i', 'o', 'u']
         no_of_vowels = 0
         for vowel in vowels:
             while True:
-                # if the vowel exist it will keep removing it 
+                # if the vowel exist it will keep removing it
                 if vowel in word:
                     no_of_vowels += 1
-                    word = word.replace(vowel,'',1)
+                    word = word.replace(vowel, '', 1)
                 else:
                     break
         print(f"{original} has {no_of_vowels} vowels ")
+
+
 if __name__:
     # find_quotiant(12, 4)
     # sum_of_four()
@@ -846,5 +873,5 @@ if __name__:
     # convertToHexa()
     # ascending_order()
     # pressure_temp_warning()
-    # division_mod_without_operator() 
-    # count_vowels(['Apple', 'Orange', 'aam'])
+    # division_mod_without_operator()
+    # count_vowels(['Apple', 'Orange', 'aam']
